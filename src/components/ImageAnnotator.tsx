2317b2010e631e7b7e19d8f8a62d8840a46c394e
Julius Reade <julius.reade@fmclarity.com>
2019-09-02 15:09:00 +1000

    done

--- src/components/ImageAnnotator.tsx
+++ src/components/ImageAnnotator.tsx
@@ -1,34 +1,46 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { Stage, Layer, Image } from 'react-konva';
+import Konva from 'konva';
+import useImage from 'use-image';
 
 import { Link } from 'react-router5';
 import { ROUTE_TITLE } from '../router';
 
-import { Surface, Group, Image, Layer } from 'react-canvas';
 
-import { getMousePos } from '../util/annotateImageDatabase';
+// import { getMousePos } from '../util/annotateImageDatabase';
 
-const Payslip:FC<PropTypes.IImageAnnotatorProps> = ({ images }) => {
+const LoadedImage = () => {
+  return ;
+};
 
-  const image = images[0];
+const ImageAnnotator:FC<PropTypes.IImageAnnotatorProps> = ({ images }) => {
+
+
+  useEffect(() => {
+    const [image] = useImage('https://konvajs.org/assets/lion.png');
+  }
 
   return (
     <div className='image__annotator'>
-      <Surface width={window.innerWidth} height={window.innerHeight} left={0} top={0}>
-        <Group>
+      <Stage width={window.innerWidth} height={window.innerHeight} left={0} top={0}>
+        <Layer>
           <Image
-            src={image.url}
-            onTouchStart={(event: MouseEvent) => {
-              const pos = getMousePos(this, event)
-              console.log(pos);
+            image={image}
+            onDragStart={(event: MouseEvent) => {
+
+              // const pos = getMousePos(this, event)
+              // console.log(pos);
+            }}
+            onDragEnd={(event: MouseEvent) => {
+
             }}
           />
-        </Group>
-        <Layer/>
-      </Surface>
+        </Layer>
+      </Stage>
     </div>
   );
 }
 
 
 
-export default Payslip;
+export default ImageAnnotator;
